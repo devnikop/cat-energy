f2c509d815c63fab1ef204444cb7cf99d177bd15
Nikolai <petrov.petrov.ak@gmail.com>
2018-11-04 13:23:02 +0300

    add image optimizitions

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+var server = require("browser-sync").create();
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -34,6 +36,22 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/imgtemp"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/imgtemp/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/imgtemp"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
